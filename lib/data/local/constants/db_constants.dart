afe5c95add4d155ea04f7872b8db84700006af93
Xuan Dien <xdienw@gmail.com>
2025-02-18 21:42:34 +0700

    chore: add cms submodule and update database constants to 'app'

--- lib/data/local/constants/db_constants.dart
+++ lib/data/local/constants/db_constants.dart
@@ -2,11 +2,8 @@ class DBConstants {
   DBConstants._();
 
   // Store Name
-  static const String STORE_NAME = 'demo';
+  static const String STORE_NAME = 'app';
 
   // DB Name
-  static const DB_NAME = 'demo.db';
-
-  // Fields
-  static const FIELD_ID = 'id';
+  static const DB_NAME = 'app.db';
 }
