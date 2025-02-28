c4d3ad7104542caca29ab9300fa436740699eb5c
Xuan Dien <xdienw@gmail.com>
2025-02-28 22:27:22 +0700

    refactor: remove environment variable handling from main and simplify app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:app_config/configured_app.dart';
 import 'package:mix_fit/di/service_locator.dart';
 import 'package:mix_fit/presentation/my_app.dart';
 import 'package:flutter/material.dart';
@@ -10,19 +9,11 @@ import 'utils/routes/module_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  const String environment =
-      String.fromEnvironment('ENV', defaultValue: 'development');
-
   await setPreferredOrientations();
   await ServiceLocator.configureDependencies();
   await ModuleManager.instance.initialize();
   await ModuleManager.instance.registerDependencies(getIt);
-  runApp(
-    ConfiguredApp(
-      environment: environment,
-      builder: (context) => MyApp(),
-    ),
-  );
+  runApp(MyApp());
 }
 
 Future<void> setPreferredOrientations() {
